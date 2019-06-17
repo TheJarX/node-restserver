36db89eb7f26bea18d946d03098f51d3a881ff5d
Gerard Altamirano <galtamiranor@outlook.es>
2019-06-17 01:27:27 -0500

    agregadas variables de desarrollo

--- server/config/config.js
+++ server/config/config.js
@@ -14,8 +14,8 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 // ============================
 //  DB
 // ============================
-// let urlDb = process.env.NODE_ENV =='dev'?
-            // 'mongodb://localhost:27017/cafe':
-            let urlDb = 'mongodb+srv://unclejoe:rEsgvsAUpRt2RlAm@cluster0-lbzdk.mongodb.net/cafe';
+let urlDb = process.env.NODE_ENV =='dev'?
+            'mongodb://localhost:27017/cafe':
+            'mongodb+srv://unclejoe:rEsgvsAUpRt2RlAm@cluster0-lbzdk.mongodb.net/cafe';
 
 process.env.URLDB = urlDb;
